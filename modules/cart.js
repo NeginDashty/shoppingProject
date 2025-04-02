4df173770cc06b4d1c395bca00a6710fe82702f0
neginDashty <negindashti.pilehroud@gmail.com>
2025-04-02 16:09:37 +0330

    Events in cart

--- modules/cart.js
+++ modules/cart.js
@@ -4,14 +4,19 @@ class Cart{
         this.price=price;
         this.products=[];
         this.toShow=[];
+        this.parent.addEventListener('click',this);
     }
-    showProducts(){
-    this.toShow=[...new Set(this.products)];
 
-    this.toShow.forEach((product)=>{
-        const qty=this.products.filter((p)=>p.id===product.id).length;
-        this.createCard(product,qty);
-    })
+    showProducts(){
+        this.parent.innerHTML = ""; // ✅ Clear previous cart content
+
+        this.toShow = [...new Set(this.products)];
+    
+        this.toShow.forEach((product) => {
+
+            const qty = this.products.filter((p) => p.id === product.id).length;
+            this.createCard(product, qty);
+        });
     
 
     }
@@ -61,6 +66,42 @@ class Cart{
         </div>
         `
         return controlJsx;
+    }
+    handleEvent(event){
+        const tagName=event.target.tagName;
+        const id=event.target.dataset.id;
+        const type=event.target.innerText;
+        if (tagName !== "BUTTON") return;
+        switch (type) {
+           case "+":
+            this.increase(id);
+           break;
+
+           case("-"):
+           this.decrease(id);
+           break;
+
+           case("Remove") :
+           this.remove(id);
+           break;
+           
+        }
+    }
+
+    increase( id ){
+        const product=this.products.find(p=>p.id=== +id);
+        this.products.push(product);
+        this.showProducts();
+    }
+    decrease( id ){
+        const index=this.products.findIndex((p)=>p.id = id);
+        this.products.splice(index,1);
+        this.showProducts();
+    }
+    remove( id ){
+        const newProducts=this.products.filter((p)=> p.id !== +id);
+        this.products=newProducts;
+        this.showProducts();
     }
 
 
